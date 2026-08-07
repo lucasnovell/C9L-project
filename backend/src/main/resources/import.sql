1772d33bc5180685c4d7cfc7d68613baae60f8de
vCoelh0 <coelhocomercial2k@gmail.com>
2026-08-06 22:52:09 -0300

    chore: change product images

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -10,42 +10,23 @@ INSERT INTO tb_user (name, email, password, role) VALUES ('Administrador', 'admi
 -- PRODUTOS
 -- ============================================
 
-INSERT INTO tb_product (name, description, price, image) VALUES ('Notebook Dell Inspiron 15', 'Notebook Intel Core i5, 16GB RAM, SSD 512GB', 4599.90, 'https://picsum.photos/400?random=1');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Mouse Logitech G502', 'Mouse Gamer RGB 25600 DPI', 299.90, 'https://picsum.photos/400?random=2');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Teclado Mecanico Redragon Kumara', 'Switch Blue ABNT2', 249.90, 'https://picsum.photos/400?random=3');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Monitor LG UltraGear 24', 'Monitor Gamer Full HD 144Hz', 1099.90, 'https://picsum.photos/400?random=4');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Headset HyperX Cloud II', 'Headset Gamer 7.1', 449.90, 'https://picsum.photos/400?random=5');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('SSD Kingston NV2 1TB', 'SSD NVMe PCIe Gen4', 399.90, 'https://picsum.photos/400?random=6');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Placa de Video RTX 4060', 'NVIDIA GeForce RTX 4060 8GB', 2399.90, 'https://picsum.photos/400?random=7');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Processador AMD Ryzen 5 5600', 'Processador AMD Ryzen 5 5600 6-Core', 899.90, 'https://picsum.photos/400?random=8');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Memoria Kingston Fury 16GB', 'DDR4 3200MHz', 269.90, 'https://picsum.photos/400?random=9');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Gabinete Corsair 4000D', 'Mid Tower com vidro temperado', 649.90, 'https://picsum.photos/400?random=10');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('iPhone 15 128GB', 'Smartphone Apple', 5799.90, 'https://picsum.photos/400?random=11');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Samsung Galaxy S24', 'Smartphone Samsung 256GB', 4999.90, 'https://picsum.photos/400?random=12');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Smart TV Samsung 55', 'Smart TV 4K UHD Crystal', 2899.90, 'https://picsum.photos/400?random=13');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Echo Dot 5 Geracao', 'Assistente Virtual Alexa', 349.90, 'https://picsum.photos/400?random=14');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('PlayStation 5 Slim', 'Console Sony PlayStation 5 Slim', 3999.90, 'https://picsum.photos/400?random=15');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Xbox Series X', 'Console Microsoft Xbox Series X', 4299.90, 'https://picsum.photos/400?random=16');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Nintendo Switch OLED', 'Console Nintendo Switch OLED', 2299.90, 'https://picsum.photos/400?random=17');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Kindle Paperwhite', 'Leitor Digital Amazon Kindle', 699.90, 'https://picsum.photos/400?random=18');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('Apple Watch Series 10', 'Smartwatch Apple', 3299.90, 'https://picsum.photos/400?random=19');
-
-INSERT INTO tb_product (name, description, price, image) VALUES ('AirPods Pro 2', 'Fones Bluetooth Apple', 1899.90, 'https://picsum.photos/400?random=20');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Notebook Dell Inspiron 15', 'Notebook Intel Core i5, 16GB RAM, SSD 512GB', 4599.90, 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Mouse Logitech G502', 'Mouse Gamer RGB 25600 DPI', 299.90, 'https://images.unsplash.com/photo-1527814050087-3793815479db?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Teclado Mecanico Redragon Kumara', 'Switch Blue ABNT2', 249.90, 'https://images.unsplash.com/photo-1595044426077-d36d9236d54a?q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Monitor LG UltraGear 24', 'Monitor Gamer Full HD 144Hz', 1099.90, 'https://images.unsplash.com/photo-1615750260764-643b0f01a0af?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Headset HyperX Cloud II', 'Headset Gamer 7.1', 449.90, 'https://images.unsplash.com/photo-1660391532247-4a8ad1060817?q=80&w=581&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+INSERT INTO tb_product (name, description, price, image) VALUES ('SSD Kingston NV2 1TB', 'SSD NVMe PCIe Gen4', 399.90, 'https://plus.unsplash.com/premium_photo-1721133221361-4f2b2af3b6fe?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Placa de Video RTX 4060', 'NVIDIA GeForce RTX 4060 8GB', 2399.90, 'https://images.unsplash.com/photo-1555618254-84e2cf498b01?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Processador AMD Ryzen 5 5600', 'Processador AMD Ryzen 5 5600 6-Core', 899.90, 'https://images.unsplash.com/photo-1591799265444-d66432b91588?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Memoria Kingston Fury 16GB', 'DDR4 3200MHz', 269.90, 'https://images.unsplash.com/photo-1562976540-1502c2145186?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Gabinete Corsair 4000D', 'Mid Tower com vidro temperado', 649.90, 'https://images.unsplash.com/photo-1587202372634-32705e3bf49c?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('iPhone 15 128GB', 'Smartphone Apple', 5799.90, 'https://images.unsplash.com/photo-1695048132832-b41495f12eb4?q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Samsung Galaxy S24', 'Smartphone Samsung 256GB', 4999.90, 'https://images.unsplash.com/photo-1706832608032-61cced969d6a?q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Smart TV Samsung 55', 'Smart TV 4K UHD Crystal', 2899.90, 'https://images.unsplash.com/photo-1593359677879-a4bb92f829d1?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Echo Dot 5 Geracao', 'Assistente Virtual Alexa', 349.90, 'https://images.unsplash.com/photo-1543512214-318c7553f230?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('PlayStation 5 Slim', 'Console Sony PlayStation 5 Slim', 3999.90, 'https://images.unsplash.com/photo-1606813907291-d86efa9b94db?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Xbox Series X', 'Console Microsoft Xbox Series X', 4299.90, 'https://images.unsplash.com/photo-1621259182978-fbf93132d53d?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Nintendo Switch OLED', 'Console Nintendo Switch OLED', 2299.90, 'https://images.unsplash.com/photo-1578303512597-81e6cc155b3e?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Kindle Paperwhite', 'Leitor Digital Amazon Kindle', 699.90, 'https://images.unsplash.com/photo-1662306477863-19133c2b2d39?q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('Apple Watch Series 10', 'Smartwatch Apple', 3299.90, 'https://images.unsplash.com/photo-1434493789847-2f02dc6ca35d?auto=format&fit=crop&w=800&q=80');
+INSERT INTO tb_product (name, description, price, image) VALUES ('AirPods Pro 2', 'Fones Bluetooth Apple', 1899.90, 'https://images.unsplash.com/photo-1590658268037-6bf12165a8df?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
