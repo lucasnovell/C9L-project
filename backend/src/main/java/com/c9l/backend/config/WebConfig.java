ed6bba695a56264abb96a2d6fd581b8fdf0eb540
vCoelh0 <coelhocomercial2k@gmail.com>
2026-09-09 23:09:06 -0300

    chore: add vercel domain on webconfig

--- backend/src/main/java/com/c9l/backend/config/WebConfig.java
+++ backend/src/main/java/com/c9l/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501", "http://localhost:5501", "http://localhost:3000" ) 
+        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501", "http://localhost:5501", "http://localhost:3000", "https://c9l-project.vercel.app" ) 
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .exposedHeaders("*")
